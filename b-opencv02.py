c44a252460031b76c5ba81b12abf8fbef9fcc224
Hiroshi Igaki <github@musictale.net>
2018-02-26 15:54:52 +0900

    skin color detect

--- b-opencv02.py
+++ b-opencv02.py
@@ -6,6 +6,8 @@ import numpy as np
 WINNAME = "OpenCV Sample 01-a"
 WIDTH = 640
 HEIGHT = 480
+lower = np.array([0, 48, 80], dtype = "uint8")
+upper = np.array([20, 255, 255], dtype = "uint8")
 
 if __name__ == '__main__':
     cv2.namedWindow(WINNAME)
@@ -18,8 +20,17 @@ if __name__ == '__main__':
         data = np.fromstring(imgStream.getvalue(), dtype=np.uint8) # translate numpy
         frame = cv2.imdecode(data, 1) #data to image
         
-        #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        cv2.imshow(WINNAME, cv2.resize(frame, (640,480)))
+        image = np.copy(frame)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hueMat = cv2.inRange(hsv, lower, upper)
+        kernel = np.ones((3,3),np.uint8)
+
+        hueMat = cv2.erode(hueMat,kernel,iterations = 3)
+        hueMat = cv2.dilate(hueMat,kernel,iterations = 6)
+        hueMat = cv2.erode(hueMat,kernel,iterations = 3)
+
+        image[hueMat == 255] = (0, 255, 0)
+        cv2.imshow(WINNAME, image)
 
         key = cv2.waitKey(10)
         if key%256 == ord('q'):
