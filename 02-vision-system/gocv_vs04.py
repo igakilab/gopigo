bf97dfd766ccf95b91799248043cfd9f6645ed9d
Hiroshi Igaki <github@musictale.net>
2018-03-21 14:55:25 +0900

    add orientation function

--- 02-vision-system/gocv_vs04.py
+++ 02-vision-system/gocv_vs04.py
@@ -61,7 +61,6 @@ class vision_system:
                 self.target1 = vs_marker[1:]
                 self.updated = datetime.datetime.now()
                 #print("target1: "+str(self.target1))
-        
 
 class gopigo_control:
     def __init__(self,screen):
@@ -86,6 +85,21 @@ class gopigo_control:
         degree_of_obj1 = numpy.rad2deg(math.atan2(obj1[3],obj1[2]))
         obj1_to_obj2 = degree_to_obj2 - degree_of_obj1
         return obj1_to_obj2
+    
+    # Angle for turning gopigo(obj1) toward an orientation of the obj2
+    def calcOrientation(self,obj1,obj2):
+        degree_of_obj1_target = numpy.rad2deg(math.atan2(obj2[3]*-1,obj2[2]*-1))
+        degree_of_obj1 = numpy.rad2deg(math.atan2(obj1[3],obj1[2]))
+        obj1_face_obj2 = degree_of_obj1_target - degree_of_obj1
+        return obj1_face_obj2
+        
+    # change the value of gopigo_to_target into the range of +-180 degree.
+    def calc_gopigo_degree(self,angle):
+        if(angle > 180):
+            angle = angle -360
+        elif (angle < -180):
+            angle = 360 + angle
+        return angle
 
     # my_gopigo and target have position and orientation information from vision system.
     # gopigo move to the target position
@@ -94,24 +108,28 @@ class gopigo_control:
         if len(my_gopigo)<4 or len(target)<4:
             return
             
-        gopigo_to_target_angle = self.calcAngle(my_gopigo,target)
+        gopigo_to_target_angle = self.calc_gopigo_degree(self.calcAngle(my_gopigo,target))
         gopigo_to_target_px = self.calcDistance(my_gopigo,target)
-        
-        # change the value of gopigo_to_target into the range of +-180 degree.
-        if(gopigo_to_target_angle > 180):
-            gopigo_to_target_angle = gopigo_to_target_angle -360
-        elif (gopigo_to_target_angle < -180):
-            gopigo_to_target_angle = 360 + gopigo_to_target_angle
+        gopigo_face_target_angle = self.calc_gopigo_degree(self.calcOrientation(my_gopigo,target))
+
         self.screen.move(1,0)
         self.screen.clrtoeol()
         self.screen.addstr(1,0,"gopigo_to_target_angle:"+str(gopigo_to_target_angle))
+        self.screen.move(2,0)
+        self.screen.clrtoeol()
+        self.screen.addstr(2,0,"gopigo_to_target_px:"+str(gopigo_to_target_px))
+        self.screen.move(3,0)
+        self.screen.clrtoeol()
+        self.screen.addstr(3,0,"gopigo_face_target_angle:"+str(gopigo_face_target_angle))
         
         # if marker info is not updated in 0.5 seconds, gopigo will stop.
-        if abs(gopigo_to_target_angle) > 10 and (datetime.datetime.now() - updated_time).total_seconds()*1000 < 500:
+        if abs(gopigo_to_target_angle) > 10 and gopigo_to_target_px > 50 and (datetime.datetime.now() - updated_time).total_seconds()*1000 < 500:
             self.pi.turn_degrees(gopigo_to_target_angle,blocking=False)
-        elif self.calcDistance(my_gopigo,target) > 50:
+        elif gopigo_to_target_px > 50:
             # 4.5 is dependent on the webcamera location of the vision system
             self.pi.drive_degrees(gopigo_to_target_px*4.5,blocking=False)
+        elif gopigo_face_target_angle > 10:
+            self.pi.turn_degrees(gopigo_face_target_angle,blocking=False)
         else:
             self.pi.stop()
 
@@ -134,9 +152,9 @@ if __name__ == "__main__":
         if len(vs.target1) < 4:
             continue
         shoot_pos = copy.deepcopy(vs.target1)
-        #150 indicates shoot_pos is 150 px in front of the target1
-        shoot_pos[0] = shoot_pos[0]+150*shoot_pos[2]
-        shoot_pos[1] = shoot_pos[1]+150*shoot_pos[3]
+        #200 indicates shoot_pos is 200 px in front of the target1
+        shoot_pos[0] = shoot_pos[0]+200*shoot_pos[2]
+        shoot_pos[1] = shoot_pos[1]+200*shoot_pos[3]
         gpgc.move(vs.shooter,shoot_pos,vs.updated)
         w = stdscr.getch() #non blocking, getch() returns int value
         if w==ord('q'):
